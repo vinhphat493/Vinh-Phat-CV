cc00b3f5768717e0d1e9763c6347eeb9626061f6
“phat” <hongthjx01@gmail.com>
2019-01-06 01:45:11 +0700

    My CV

--- dist/project/ProjectElearning/app/Models/khoahoc.js
+++
@@ -1,8 +0,0 @@
-function KhoaHoc (maKhoaHoc,tenKhoaHoc,moTa,hinhAnh,luotXem,nguoiTao){
-    this.MaKhoaHoc = maKhoaHoc;
-    this.TenKhoaHoc = tenKhoaHoc;
-    this.MoTa = moTa;
-    this.HinhAnh = hinhAnh;
-    this.LuotXem = luotXem;
-    this.NguoiTao = nguoiTao;
-}
--- dist/project/ProjectElearning/app/Models/nguoidung.js
+++
@@ -1,9 +0,0 @@
-function NguoiDung(taiKhoan,matKhau,hoTen,email,soDT,maLoaiNguoiDung,tenLoaiNguoiDung){
-    this.TaiKhoan = taiKhoan;
-    this.MatKhau = matKhau;
-    this.HoTen = hoTen;
-    this.Email = email;
-    this.SoDT = soDT;
-    this.MaLoaiNguoiDung = maLoaiNguoiDung;
-    this.TenLoaiNguoiDung = tenLoaiNguoiDung;
-}
